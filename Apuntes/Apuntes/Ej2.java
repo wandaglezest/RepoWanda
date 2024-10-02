7afbd043ab64cabdebaa5d5d4c39d85b1ef08e93
Wanda <Wanda@MSI>
2024-10-02 21:31:31 +0200

    de

--- Apuntes/Apuntes/Ej2.java
+++ Apuntes/Apuntes/Ej2.java
@@ -4,13 +4,9 @@ public class Ej2 {
 	public static void main (String []args) {
 		int numero1 = 7;
 		int numero2 = 5;
+		int suma= numero1+numero2;
 		
-		System.out.print("Tenemos el número ");
-		System.out.println(numero1);
-		System.out.print("Tenemos el número ");
-		System.out.println(numero2);
-		System.out.print("La suma de los números es: ");
-		System.out.println(numero2 + numero1);
+		System.out.print("Tenemos el número "+ numero1 + " y el número " + numero2 + ".El resultado de la suma es:" + suma + ".");
 		
 	}
 
