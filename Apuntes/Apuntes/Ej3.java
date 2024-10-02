2dfe77142f225d3122d62bc226330051cc476640
Wanda <Wanda@MSI>
2024-10-02 19:31:19 +0200

    z

---
+++ Apuntes/Apuntes/Ej3.java
@@ -0,0 +1,17 @@
+package Apuntes;
+public class Ej3 {
+	public static void main (String []args) {
+		
+		double ancho = 7.5;
+		double alto = 4.9;
+		double area = ancho * alto;
+		
+		System.out.print("El ancho es: ");
+		System.out.println(ancho);
+		System.out.print("Y el alto es: ");
+		System.out.println(alto);
+		System.out.print("El área de este rectángulo es " +area+ "cm2");
+		
+}
+
+}
