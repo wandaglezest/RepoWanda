f2dc88204d924db333447a132552b8a04de4b237
Wanda <Wanda@MSI>
2024-10-02 19:07:22 +0200

    f

--- interfaces/src/Apuntes/Ej2.java
+++ interfaces/src/Apuntes/Ej2.java
@@ -1,5 +1,17 @@
 package Apuntes;
 
 public class Ej2 {
+	public static void main (String []args) {
+		int numero1 = 7;
+		int numero2 = 5;
+		
+		System.out.print("Tenemos el número ");
+		System.out.println(numero1);
+		System.out.print("Tenemos el número ");
+		System.out.println(numero2);
+		System.out.print("La suma de los números es: ");
+		System.out.println(numero2 + numero1);
+		
+	}
 
 }
