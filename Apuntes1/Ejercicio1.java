eb4b76779e0eb932cc0b6349021d99bff351f550
Wanda <Wanda@MSI>
2024-10-02 18:44:10 +0200

    x

---
+++ Apuntes1/Ejercicio1.java
@@ -0,0 +1,16 @@
+package Apuntes1;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		
+		int numero = 7;
+		double numerodecimal = 7.8;
+		String palabra = "asdjkhasdkjh";
+		
+		
+		System.out.println(numero);
+		System.out.println(numerodecimal);
+		System.out.println(palabra);
+	}
+	
+} 
---
+++ Apuntes1/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Apuntes1;
+
+public class Ejercicio2 {
+	public static void main (String []args) {
+		int numero1 = 7;
+		int numero2 = 5;
+		
+		System.out.print("Tenemos el número ");
+		System.out.println(numero1);
+		System.out.print("Tenemos el número ");
+		System.out.println(numero2);
+	}
+
+}
