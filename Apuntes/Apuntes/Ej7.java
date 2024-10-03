8062cae0f4474d43b932f09d2f75e603d357f440
Wanda <Wanda@192.168.13.22>
2024-10-03 13:28:05 +0200

    d

---
+++ Apuntes/Apuntes/Ej7.java
@@ -0,0 +1,18 @@
+package Apuntes;
+
+
+public class Ej7 {
+	public static void main (String[]args) {
+		/*Declara tres variables de tipo entero, asígnales un valor, y calcula el promedio de los tres números. Imprime el resultado.*/
+		
+		int numero1 = 4;
+		int numero2 = 6;
+		int numero3 = 8;
+		
+		
+		System.out.println("El promedio es "+ (numero1+ numero2+ numero3)/3 + ".");
+		
+	}
+	
+
+}
---
+++ Apuntes/Apuntes/Ej8.java
@@ -0,0 +1,19 @@
+package Apuntes;
+
+public class Ej8 {
+ public static void main (String[]args) {
+	 
+	 /*Declara dos variables booleanas, una con el valor true y otra con false. 
+	  * Imprime los resultados de aplicar las operaciones lógicas AND, OR y NOT a estas variables.
+	  */
+	 
+	 boolean uno = true;
+	 boolean dos = false;
+	 int numero = 1;
+	 if (numero < 5) {
+		
+	}
+	
+ }
+ 
+}
